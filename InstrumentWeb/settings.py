2723ffe8f93ab293791061e25c2320001527a57d
H.Quynh <shodakima@gmail.com>
2025-11-29 13:18:45 +0700

    Update feature by Quynh1

--- InstrumentWeb/settings.py
+++ InstrumentWeb/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'InstrumentWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / "static"]  # load CSS/JS chung
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
