605693ed743df3eb3b1bf41e0a8c5ec7376e2185
saoson095-dotcom <saoson095@gmail.com>
2025-12-03 22:36:35 +0700

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Thêm trường địa chỉ giao hàng
+    shipping_address = models.CharField(max_length=255, blank=True, null=True)
+    # Thêm trường số điện thoại
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
