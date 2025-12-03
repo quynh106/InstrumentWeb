b729ac7ab212816cf6ccd5b39d037c8e07a4eea7
saoson095-dotcom <saoson095@gmail.com>
2025-12-03 22:39:18 +0700

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm # Bạn phải tạo file forms.py
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save() # Lưu User mới vào DB
+            return redirect('login') # Chuyển hướng đến trang login
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
