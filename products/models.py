d17a7f332bf0b002ec56b8b09444c0b69dc0a5de
H.Quynh <shodakima@gmail.com>
2025-12-04 15:54:05 +0700

    fix

--- products/models.py
+++ products/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Avg
 
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from product.models import Product   # import Product do nhóm khác làm
+from products.models import Product   # import Product do nhóm khác làm
 
 # 1. Giỏ hàng
 class Cart(models.Model):
