b2768f8edb1f23a99f5bf07b01d1e826905d4cc1
H.Quynh <shodakima@gmail.com>
2025-12-12 18:39:14 +0700

    edit product

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Category, Brand, Product, Review
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "brand", "price", "stock","image", "created_at")
+    list_filter = ("category", "brand")
+    search_fields = ("name", "description")
+    ordering = ("-created_at",)
+    list_editable = ("price", "stock","image")
+    list_per_page = 20
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "user", "rating", "created_at")
+    list_filter = ("rating", "created_at")
+    search_fields = ("product__name", "user__username", "comment")
+    list_per_page = 20
--- products/views.py
+++ products/views.py
@@ -8,7 +8,7 @@ def product_list(request):
     brand_id = request.GET.get('brand')
 
 
-    # NEW: sort option
+    # filter
     sort_by = request.GET.get('sort')
 
     if category_id in ('', 'None'):
@@ -40,14 +40,37 @@ def product_list(request):
 
 
 def product_detail(request, pk):
+    
     product = get_object_or_404(Product, pk=pk)
 
     related_products = Product.objects.filter(
         category=product.category
     ).exclude(pk=product.pk)[:6]
 
-    return render(request, 'products/product_detail.html', {
+    search_query = request.GET.get('q', '')
+    category_id = request.GET.get('category')
+    brand_id = request.GET.get('brand')
+
+
+    # filter
+    sort_by = request.GET.get('sort')
+
+    if category_id in ('', 'None'):
+        category_id = None
+    if brand_id in ('', 'None'):
+        brand_id = None
+
+    products = Product.get_filtered_products(search_query, category_id, brand_id)
+
+    context = {
         'product': product,
-        'related_products': related_products
-    })
+        'related_products': related_products,
+        'products': products,
+        'search_query': search_query,
+        'category_id': category_id,
+        'brand_id': brand_id,
+        'categories': Category.objects.all(),
+        'brands': Brand.objects.all(),
+    }
+    return render(request, 'products/product_detail.html', context)
 
