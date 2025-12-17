757b443c617cb6ca4097e158f9112cf47da7fcb5
unknown <23130393@st.hcmuaf.edu.vn>
2025-12-18 06:50:38 +0700

    Admin Panel and Order Management

--- products/admin.py
+++ products/admin.py
@@ -3,29 +3,24 @@ from .models import Category, Brand, Product, Review
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
-    search_fields = ("name",)
-
+    list_display = ['id', 'name']
+    search_fields = ['name']
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
-    search_fields = ("name",)
-
+    list_display = ['id', 'name']
+    search_fields = ['name']
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "category", "brand", "price", "stock","image", "created_at")
-    list_filter = ("category", "brand")
-    search_fields = ("name", "description")
-    ordering = ("-created_at",)
-    list_editable = ("price", "stock","image")
-    list_per_page = 20
-
+    list_display = ['id', 'name', 'category', 'brand', 'price', 'stock', 'created_at']
+    list_filter = ['category', 'brand', 'created_at']
+    search_fields = ['name', 'description']
+    list_editable = ['price', 'stock']
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ("id", "product", "user", "rating", "created_at")
-    list_filter = ("rating", "created_at")
-    search_fields = ("product__name", "user__username", "comment")
+    list_display = ['id', 'product', 'user', 'rating', 'created_at']
+    list_filter = ['rating', 'created_at']
+    search_fields = ['product__name', 'user__username', 'comment']
     list_per_page = 20
