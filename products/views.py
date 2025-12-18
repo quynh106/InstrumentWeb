d8d1e526869bf27ac0f50b0fad11ffefa4722429
H.Quynh <shodakima@gmail.com>
2025-12-18 12:18:48 +0700

    count reviews

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product,Category, Brand, Review
-from django.db.models import Avg
+from django.db.models import Avg, Count
 from django.shortcuts import redirect
 
 
@@ -8,7 +8,6 @@ def product_list(request):
     search_query = request.GET.get('q', '')
     category_id = request.GET.get('category')
     brand_id = request.GET.get('brand')
-
 
     # filter
     sort_by = request.GET.get('sort')
@@ -20,15 +19,14 @@ def product_list(request):
 
     products = Product.get_filtered_products(search_query, category_id, brand_id)
 
-   
-
-
     # APPLY SORTING
     if sort_by == "price_asc":
         products = products.order_by("price")
     elif sort_by == "price_desc":
         products = products.order_by("-price")
    
+    # 5 new products
+    new_products = Product.objects.order_by('-created_at')[:5]
 
     context = {
         'products': products,
@@ -37,8 +35,12 @@ def product_list(request):
         'brand_id': brand_id,
         'categories': Category.objects.all(),
         'brands': Brand.objects.all(),
+        'new_products': new_products,
     }
     return render(request, 'products/product_list.html', context)
+
+
+
 
 
 def product_detail(request, pk):
@@ -65,14 +67,33 @@ def product_detail(request, pk):
     products = Product.get_filtered_products(search_query, category_id, brand_id)
 
 # review
-    reviews = product.reviews.all()
+    all_reviews = product.reviews.all()
 
+# Đếm số review mỗi sao
+    rating_counts = (
+        all_reviews.values("rating").annotate(count=Count("id"))
+)
+
+    # Tạo list 5 → 1 (KHÔNG cần get_item)
+    star_filters = []
+    counts_map = {item["rating"]: item["count"] for item in rating_counts}
+
+    for i in range(5, 0, -1):
+        star_filters.append({
+        "star": i,
+        "count": counts_map.get(i, 0)
+    })
+
+# Lọc review để hiển thị
+    reviews = all_reviews
     star = request.GET.get("star")
     if star and star != "all":
         reviews = reviews.filter(rating=star)
 
-    average_rating = reviews.aggregate(avg=Avg("rating"))["avg"] or 0
-    total_reviews = reviews.count()
+# Trung bình & tổng
+    average_rating = all_reviews.aggregate(avg=Avg("rating"))["avg"] or 0
+    total_reviews = all_reviews.count()
+
 
     context = {
         'product': product,
@@ -86,8 +107,12 @@ def product_detail(request, pk):
         "reviews": reviews,
         "average_rating": round(average_rating, 1),
         "total_reviews": total_reviews,
+        "star_filters": star_filters,
     }
     return render(request, 'products/product_detail.html', context)
+
+
+
 
 def review_product(request, product_id):
     product = get_object_or_404(Product, id=product_id)
