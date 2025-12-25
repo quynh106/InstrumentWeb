5e1ee650426f27dc8f8e6aa896467eadd4cfbfe5
saoson095-dotcom <saoson095@gmail.com>
2025-12-25 13:23:39 +0700

    Update urls.py of Users

--- users/urls.py
+++ users/urls.py
@@ -1,1 +1,13 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    # Đường dẫn cho trang Đăng ký: http://127.0.0.1:8000/users/register/
+    path('register/', views.register, name='register'),
+    
+    # Đường dẫn cho trang Đăng nhập: http://127.0.0.1:8000/users/login/
+    path('login/', views.login_view, name='login'),
+    
+    # Đường dẫn cho trang Đăng xuất: http://127.0.0.1:8000/users/logout/
+    path('logout/', views.logout_view, name='logout'),
+]
